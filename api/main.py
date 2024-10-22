a43e562a783b98d4e7874838f6734b084f8399ce
Nicolas Calafiore <121151093+NicolasCalafiore@users.noreply.github.com>
2024-10-22 16:01:01 -0400

    fastAPI-users implementation

--- api/main.py
+++ api/main.py
@@ -1,48 +1,22 @@
 from fastapi import FastAPI
-import sqlalchemy as sa
-from sqlalchemy.testing.suite.test_reflection import metadata
-from websockets.asyncio.client import connect
-
+from api.auth import app as auth_app
+from fastapi_users import models
+from fastapi import FastAPI, Depends
+from api.auth import app as auth_app, user_db
 app = FastAPI()
+app.mount("/", auth_app) # Allows all routes in auth_app to be accessed from the root of the app
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-# Server Stuff that I'm not 100% sure belongs
-# 2 Versions of SQL Alchemy Installed !!!
+@app.post("/add_user")
+async def add_user(user: models.BaseUserCreate, user_db=Depends(user_db)):
+    await user_db.create(user)
+    return {"message": "User created successfully"}
 
-account_db = sa.create_engine("Accounts")
-account_db_con = account_db.connect()
-account_meta = account_db_con.MetaData()
-user_table = sa.table(
-    "users",
-    account_meta,
-    sa.Column("id", sa.Integer, primary_key=True),
-    sa.Column("name", sa.String),
-    sa.Column("key", sa.String),
-)
-
-def insert_user(username, email, password):
-    query = user_table.insert().values(name=username, email=email, password=password)
-    account_db_con.execute(query)
-
-def select_user(username):
-    query = user_table.select().where(user_table.c.name == username)
-    result = account_db_con.execute(query).fetchone()
-    return result
-
-def account_db_main_debug():
-    metadata.create_all(account_db_con)
-    insert_user("admin", "<EMAIL>", "password")
-    print(select_user("admin"))
-    account_db_con.close()
-
-def account_authentication(name, password) -> bool:
-    query = user_table.select().where(user_table.c.name == name)
-    result = account_db_con.execute(query).fetchone()
-    if result and result['key'] == password:
-        return True
-    return False
-
+@app.get("/user_exists/{username}")
+async def user_exists(username: str, user_db=Depends(user_db)) -> bool:
+    user = await user_db.get_by_username(username)
+    return user is not None
