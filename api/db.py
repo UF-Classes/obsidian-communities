c05780d403d17a7e28f9ce49b84bce2f79be8dba
Nicolas Calafiore <121151093+NicolasCalafiore@users.noreply.github.com>
2024-10-24 20:06:07 -0400

    Error-Free Backend, User Registration Implemented

--- api/db.py
+++ api/db.py
@@ -1,10 +1,13 @@
+import asyncio
 from typing import AsyncGenerator
 
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import sessionmaker
+
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 Base: DeclarativeMeta = declarative_base()
@@ -28,5 +31,15 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
         yield session
 
 
+async def get_user_by_email(email : str):
+    async with async_session_maker() as session:
+        async with session.begin():
+            result = await session.execute(select(User).where(User.email == email))
+            return result.fetchone()
+
+
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
     yield SQLAlchemyUserDatabase(session, User)
+
+
+asyncio.create_task(create_db_and_tables())
--- api/main.py
+++ api/main.py
@@ -1,38 +1,24 @@
-from fastapi import FastAPI, HTTPException
-from fastapi_users import models
-from fastapi import FastAPI, Depends
-from starlette import status
+from api.app import app
+from api.schemas import UserRead
+from api.users import get_user_manager
+from api.db import get_user_db, get_user_by_email
 
-from api.schemas import UserRead, UserCreate, UserUpdate
-from users import auth_backend, current_active_user, fastapi_users, UserManager, get_user_manager
-from db import create_db_and_tables
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-app = FastAPI()
 
-# TO DO: Are these routers necessary?
-app.include_router(
-    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
-)
-app.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth",
-    tags=["auth"],
-)
-app.include_router(
-    fastapi_users.get_reset_password_router(),
-    prefix="/auth",
-    tags=["auth"],
-)
-app.include_router(
-    fastapi_users.get_verify_router(UserRead),
-    prefix="/auth",
-    tags=["auth"],
-)
-app.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
-    prefix="/users",
-    tags=["users"],
-)
+
+# Email
+@app.get("/users/exists/{user_email}") # Make sure no repeats
+async def create_user(user_email : str):
+    exists = await get_user_by_email(user_email)
+    if exists is None:
+        return {"exists": False}
+    else:
+        return {"exists": True}
+
+
 
 # HTTP Methods
 # POST
@@ -41,23 +27,3 @@ app.include_router(
 # DELETE
 
 #login --> check if admin
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.post("/users", response_model=UserRead) # Make sure no repeats
-async def create_user(user: UserCreate, user_manager: UserManager = Depends(get_user_manager)):
-    try:
-        created_user = await user_manager.create(user)
-        return created_user
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=str(e)
-        )
-
-# Review if necessary
-@app.on_event("startup")
-async def startup():
-    print("Server starting up")
-    await create_db_and_tables()
