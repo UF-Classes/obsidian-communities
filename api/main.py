48dff2f3321846279c7268dda87b3bce39d8741b
Asinphi <88jliu88@gmail.com>
2024-10-29 19:46:18 -0400

    refactor(api): exists route returns more user data

--- api/main.py
+++ api/main.py
@@ -3,22 +3,27 @@ from api.schemas import UserRead
 from api.users import get_user_manager
 from api.db import get_user_db, get_user_by_email
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-
 # Email
-@app.get("/users/exists/{user_email}") # Make sure no repeats
-async def create_user(user_email : str):
-    exists = await get_user_by_email(user_email)
-    if exists is None:
+@app.get("/users/exists/{user_email}")  # Make sure no repeats
+async def create_user(user_email: str):
+    matching_users = await get_user_by_email(user_email)
+    if matching_users is None:
         return {"exists": False}
     else:
-        return {"exists": True}
-
-
+        user = matching_users[0]
+        return {
+            "exists": True,
+            "id": user.id,
+            "is_active": user.is_active,
+            "is_verified": user.is_verified,
+            "is_superuser": user.is_superuser,
+        }
 
 # HTTP Methods
 # POST
@@ -26,4 +31,4 @@ async def create_user(user_email : str):
 # PUT
 # DELETE
 
-#login --> check if admin
+# login --> check if admin
